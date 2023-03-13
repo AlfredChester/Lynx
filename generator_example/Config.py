bb2497d02fb0d0445885c8faf4e3a7e55314a847
FredB-mine <fredbao1126@gmail.com>
2023-03-13 20:07:22 +0800

    Generator_example

--- generator_example/Config.py
+++ generator_example/Config.py
@@ -1,17 +1,18 @@
 # encoding: utf-8
 
 # version 2 Config Template
-from cyaron    import *         # cyaron
-from libs.glib import ToolSet   # Toolset
+from cyaron import *  # cyaron
+from libs.glib import ToolSet  # Toolset
 
-# Constant Definations
-version  = '2.0.0'
+# Constant Definitions
+version = '2.0.0'
 std_name = 'std.cpp'
 data_set = 10
 
 # Experimental Function
-no_gen   = False
-genOut   = False    # Generate Output in Gen.generator
+no_gen = False
+genOut = False  # Generate Output in Gen.generator
+
 
 # Gen class
 class Gen:
@@ -28,7 +29,8 @@ class Gen:
     #  param: data_group -> which data is being generated
     #  directly write input data to io
     #  See https://github.com/luogu-dev/cyaron/wiki/输入输出-IO
-    def generator(data_group: int, io: IO) -> None:
+    @staticmethod
+    def generator(io: IO) -> None:
         a = randint(Gen.static.INT_MIN, Gen.static.INT_MAX)
         b = randint(Gen.static.INT_MIN, Gen.static.INT_MAX)
         io.input_writeln(a, b)
