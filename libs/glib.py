73d7cd5134f7e57609108f1c579c027a4b3991c6
FredB-mine <fredbao1126@gmail.com>
2023-03-13 20:07:25 +0800

    glibs

--- libs/glib.py
+++ libs/glib.py
@@ -2,17 +2,21 @@ from cyaron import *
 from typing import *
 from string import *
 
+
 class ToolSet:
+    INT_MAX = (1 << 31) - 1
+    INT_MIN = -INT_MAX - 1
     general_mod = int(1e9 + 7)
-    def genRandVector(io: IO, len, data_range, allow_same = True):
+
+    def genRandVector(io: IO, vec_len: int, data_range, allow_same=True):
         if isinstance(data_range, int):
             data_range = (0, data_range)
-        to_write = Vector.random(len, [data_range], int(allow_same))
+        to_write = Vector.random(vec_len, [data_range], int(allow_same))
         for item in to_write:
             io.input_write(item)
         io.input_writeln()
 
-    def genRandStr(siz: int, gen_src = ascii_letters) -> str:
+    def genRandStr(siz: int, gen_src=ascii_letters) -> str:
         ret = ''
         for _ in range(siz):
             ret += choice(gen_src)
