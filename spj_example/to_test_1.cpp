fa7e682ad50457e4be7aa6adc1280588f61756aa
FredB-mine <fredbao1126@gmail.com>
2023-03-19 11:57:13 +0800

    spj_example

---
+++ spj_example/to_test_1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long a, b;
+// input two numbers
+// print two numbers that has a sum of a + b
+int main(int argc, char const *argv[]) {
+    scanf("%lld%lld", &a, &b);
+    printf("%lld %lld\n", a, b);
+    return 0;
+}
