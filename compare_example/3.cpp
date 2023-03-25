44bd988c52257fbfcebb381c06b5fc92c7881945
FredB-mine <fredbao1126@gmail.com>
2023-03-25 16:33:18 +0800

    compare_example

---
+++ compare_example/3.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[]) {
+    long long a, b;
+    scanf("%lld%lld", &a, &b);
+    printf("%lld\n", a + b);
+    return 0;
+}
