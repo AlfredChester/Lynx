69a8cc3810181350532c48a36ab55bfce923fcd7
FredB-mine <fredbao1126@gmail.com>
2023-07-11 21:13:30 +0800

    Encoding bugfixes

--- spj_example/spj.cpp
+++ spj_example/spj.cpp
@@ -6,22 +6,22 @@
 
 int spj(FILE *input, FILE *user_output);
 
-void close_file(FILE *f){
-    if(f != NULL){
+void close_file(FILE *f) {
+    if (f != NULL) {
         fclose(f);
     }
 }
 
-int main(int argc, char *args[]){
+int main(int argc, char *args[]) {
     FILE *input = NULL, *user_output = NULL;
     int result;
-    if(argc != 3){
+    if (argc != 3) {
         printf("Usage: spj x.in x.out\n");
         return ERROR;
     }
-    input = fopen(args[1], "r");
+    input       = fopen(args[1], "r");
     user_output = fopen(args[2], "r");
-    if(input == NULL || user_output == NULL){
+    if (input == NULL || user_output == NULL) {
         printf("Failed to open output file\n");
         close_file(input);
         close_file(user_output);
@@ -30,13 +30,13 @@ int main(int argc, char *args[]){
 
     result = spj(input, user_output);
     printf("result: %d\n", result);
-    
+
     close_file(input);
     close_file(user_output);
     return result;
 }
 
-int spj(FILE *input, FILE *user_output){
+int spj(FILE *input, FILE *user_output) {
     long long a, b, c, d;
     fscanf(input, "%lld%lld", &a, &b);
     fscanf(user_output, "%lld%lld", &c, &d);
