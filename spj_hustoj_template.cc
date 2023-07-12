b6e88c1791adefe572f65c7e2498f0f1dc44e003
FredB-mine <fredbao1126@gmail.com>
2023-07-12 15:27:44 +0800

    HustOJ spj template

---
+++ spj_hustoj_template.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+#define AC 0
+#define WA 1
+#define ERROR -1
+
+int spj(FILE *input, FILE *std_output, FILE *user_output);
+
+void close_file(FILE *f) {
+    if (f != NULL) {
+        fclose(f);
+    }
+}
+
+int main(int argc, const char *args[]) {
+    FILE *input = NULL, *user_output = NULL, *std_output = NULL;
+    int result;
+    if (argc != 4) {
+        printf("Usage: spj x.in x.out user.out\n");
+        return ERROR;
+    }
+    input       = fopen(args[1], "r");
+    std_output  = fopen(args[2], "r");
+    user_output = fopen(args[3], "r");
+    if (input == NULL || user_output == NULL || std_output == NULL) {
+        printf("Failed to open output file\n");
+        close_file(input);
+        close_file(user_output);
+        close_file(std_output);
+        return ERROR;
+    }
+
+    result = spj(input, std_output, user_output);
+    printf("result: %d\n", result);
+
+    close_file(input);
+    close_file(user_output);
+    close_file(std_output);
+    return result;
+}
+using namespace std;
+int spj(FILE *input, FILE *std_output, FILE *user_output) {
+    /*
+      parameter:
+        - input, The FILE pointer of input file
+        - user_output, The FILE pointer of user's output
+        - std_output
+      return:
+        - If the answer is correct, return AC
+        - If the answer is wrong, return WA
+        - If you actively capture your own errors,
+          such as memory allocation failure, return ERROR
+      Please complete the function on your own.
+      demo:
+      int a, b;
+      while(fscanf(user_output, "%d %d", &a, &b) != EOF){
+          if(a - b != 3){
+              return WA;
+          }
+      }
+      return AC;
+    */
+    return AC;
+}
