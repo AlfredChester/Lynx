f03b72087694b05d5701cbcb8c611c8b800608b7
FredB-mine <fredbao1126@gmail.com>
2023-08-19 16:38:15 +0800

    General

--- general.py
+++ general.py
@@ -1,10 +1,13 @@
 from importlib import import_module
 from os import listdir, system
+from sys import argv
 from zipfile import *
 
 from cyaron import *
+from tqdm import tqdm
 
-from libs.glib import args as argv
+if argv[0].startswith('python'):
+    argv = argv[1:]
 
 try:
     root = argv[1]
@@ -19,7 +22,7 @@ print('Problem Root:', root)
 moduleRoot = root.replace('/', '.')
 try:
     Config = import_module(moduleRoot + '.Config')
-except FileNotFoundError:
+except Exception:
     if 'gen.py' in listdir(f'./{moduleRoot}'):
         print('Old generator found, running...')
         system(f'python3 ./{moduleRoot}/gen.py')
@@ -29,44 +32,85 @@ except FileNotFoundError:
 
 print('Read Config v' + Config.version)
 
+useConfigGen = False
 try:
     useConfigGen = Config.genOut
-except AttributeError:
-    useConfigGen = False
+except Exception:
+    pass
+
+validator = None
+try:
+    validator = Config.validator
+except Exception:
+    pass
+
+compileCommand = "g++ -O3 -g -m64 -std=c++14 -Wall -o {}/std.exe {}"
+try:
+    compileCommand = Config.cmd
+    print("Read customized compile command:", compileCommand)
+except Exception:
+    print("Can't read customized compile command, using default:")
+    print(compileCommand)
 
 zipName = root.split('/')[-1] + '.zip'
 zipFile = ZipFile(root + '/' + zipName, 'w')
 
 
-def compile_source() -> int:
-    real_cpp = root + '/' + Config.std_name
-    cmd = f"g++ -O3 -g -m64 -std=c++14 -Wall -o {root}/std.exe {real_cpp}"
+def compileCpp() -> int:
+    realCpp = root + '/' + Config.std_name
+    cmd = compileCommand.format(root, realCpp)
     print("Compile cmd: ", cmd)
     return system(cmd)
 
 
-def main(*args, **kwargs) -> int:
+def main() -> int:
     if not useConfigGen:
-        retval = compile_source()
+        retval = compileCpp()
         if retval != 0:
             print('Compile Failure')
             return 0
-    for textGroup in range(1, Config.data_set + 1):
+    if validator != None:
+        # try to restructure this part if allowed
+        retval = system(
+            "g++ -O3 -g -m64 -std=c++14 -o {}/{} {}/{}".format(
+                root, 'validator.exe', root, validator
+            )
+        )
+        if retval != 0:
+            print('Compile Failure')
+            return 0
+    bar = tqdm(range(1, Config.data_set + 1))
+    for textGroup in bar:
         # IO Object Creation
         file = IO(
-            file_prefix=root + '/', data_id=textGroup
+            file_prefix=root+'/',
+            data_id=textGroup
         )
-        # Gen Data 
+        # Gen Data
         if Config.version.split('.')[0] == '1':
             file.input_write(Config.generator(textGroup))
         else:
             ret = Config.Gen.generator(textGroup, file)
-            if ret is not None:
+            if ret != None:
                 file.input_write(ret)
-                # Gen Output
+        # Gen Output
+        bar.set_description(f'Generated Input    {textGroup}')
         if not useConfigGen:
             file.output_gen(f'{root}/std.exe')
-        print(f'Generated Testcase {textGroup}')
+        bar.set_description(f'Generated Testcase {textGroup}')
+        if validator != None:
+            retval = system(
+                f'./{root}/validator.exe {root}/{textGroup}.in'
+            )
+            if retval == 256:
+                print("Data error! Please check Config.py and validator.cpp")
+                return 0
+            elif retval == 512:
+                print("Error occurred when checking data.")
+                print("Data generation terminated.")
+                return 0
+            else:
+                bar.set_description(f'Validated Testcase {textGroup}')
         zipFile.write(
             f'{root}/{textGroup}.in', arcname=f'{textGroup}.in',
             compress_type=ZIP_DEFLATED, compresslevel=9
