be46e82e2d840a56d2508f248eec39cfe934e882
FredB-mine <fredbao1126@gmail.com>
2023-08-19 16:38:11 +0800

    Config

--- fastinit_example/Config.py
+++ fastinit_example/Config.py
@@ -2,19 +2,41 @@
 
 # version 2 Config Template
 from cyaron import *
-from libs.glib import ToolSet
+from typing import *
+from string import *
 
 # Constant Definitions
-version = '2.0.0'
-std_name = 'std.cpp'
+version = '2.2.0'
 data_set = 20
+std_name = 'std.cpp'
+validator = 'validator.cpp'
+cmd = 'g++ -O3 -g -m64 -std=c++14 -Wall -o {}/std.exe {}'
 
 # Experimental Function
 no_gen = False
-genOut = False  # Generate Output in Gen.generator
+genOut = False    # Generate Output in Gen.generator
+
+# Tool Set
 
 
-# Gen class
+class ToolSet:
+    general_mod = int(1e9 + 7)
+
+    def genRandVector(io: IO, len, data_range, allow_same=True):
+        if isinstance(data_range, int):
+            data_range = (0, data_range)
+        to_write = Vector.random(len, [data_range], int(allow_same))
+        for item in to_write:
+            io.input_write(item)
+        io.input_writeln()
+
+    def genRandStr(siz: int, gen_src=ascii_letters) -> str:
+        ret = ''
+        for _ in range(siz):
+            ret += choice(gen_src)
+        return ret
+
+
 class Gen:
     class static:
         # static vars
@@ -24,10 +46,4 @@ class Gen:
         # functions
         pass
 
-    # gen.generator:
-    #  param: data_group -> which data is being generated
-    #  directly write input data to io
-    #  See https://github.com/luogu-dev/cyaron/wiki/输入输出-IO
-    @staticmethod
-    def generator(data_group: int, io: IO) -> None:
-        pass
+    def generator(data_group: int, io: IO) -> Optional[str]:  ...
