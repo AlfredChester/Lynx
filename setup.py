c30e49b728dc25f4be41503a7c8ce2c168da7b55
FredB-mine <fredbao1126@gmail.com>
2025-01-23 19:51:34 +0800

    Add __init__.py for setuptools to find modules.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lynx",
-    version="2.0.1-beta",
+    version="2.0.1b0",
     keywords="olympic informatics testcase testcase-tools tools",
     description="lynx: Next generation high performance testcase tool set for OI / ACM contests. - 新一代信息学竞赛数据生成工具集",
     license="MIT",
